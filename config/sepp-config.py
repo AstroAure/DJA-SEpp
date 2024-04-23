f26640f85188dee3a647d57b90d4fa78ffa81837
Aurélien Genin <aurelien.genin@cube-monde.net>
2024-04-23 16:38:53 +0200

    Update sersic model to solve the fmf_stop_reason=7 issue. Created functions to better displays cutouts and spectra.

--- config/sepp-config.py
+++ config/sepp-config.py
@@ -2,11 +2,10 @@ from glob import glob
 import numpy as np
 from sourcextractor.config import *
 
-import os
-
 
 ##### THESE NEXT LINES DO SOME MORE CONFIGURATION #######
-fit_case = 'sersic' # this defines which model we want to fit
+field = 'COSMOS'
+fit_case = 'sersic_rg4' # this defines which model we want to fit
 set_engine('levmar')
 set_max_iterations(250)
 use_iterative_fitting(True)
@@ -17,22 +16,9 @@ set_meta_iteration_stop(0.01)  ## increase -> faster!
 ###################################################################################
 ################################ LOAD IMAGES ######################################
 
-field = 'GDS'
 list_of_IMG_names = glob(f"/home/aurelien/DAWN/DJA_SE++/image/{field}/cutout/*clear*sci*.fits")
 list_of_WHT_names = glob(f"/home/aurelien/DAWN/DJA_SE++/image/{field}/cutout/*clear*wht*.fits")
 list_of_PSF_names = glob(f"/home/aurelien/DAWN/DJA_SE++/psfex/{field}/*star_psf.psf")
-
-# mag_zeropoint = {'F090W': 28.02,
-#                  'F115W': 28.02,
-#                  'F150W': 28.02,
-#                  'F182M': 28.02,
-#                  'F200W': 28.02,
-#                  'F210M': 28.02,
-#                  'F277W': 26.49,
-#                  'F335M': 26.49,
-#                  'F356W': 26.49,
-#                  'F410M': 26.49,
-#                  'F444W': 26.49}
 
 mag_zeropoint = {'F090W': 28.90,
                  'F115W': 28.90,
@@ -83,54 +69,72 @@ add_output_column('APER', all_apertures)
 
 ###################################################################################
 ################################ DEFINE MODELS ####################################
-if fit_case == "sersic" :
-    # first get the source positions from the detection image
-    x, y = get_pos_parameters()
+if fit_case == "sersic_rg4" :
     
-    ## define Sersic model parameters: radius, angle, axis ratio, sersic index:
+    x,y = get_pos_parameters()
     
-    rad = FreeParameter(lambda o: o.radius, Range(lambda v, o: (.01 * v, 50 * v), RangeType.EXPONENTIAL))
-    angle = FreeParameter( lambda o: o.angle )
+    rad = FreeParameter(lambda o: o.radius, Range(lambda v, o: (.0001, 1.5*v), RangeType.EXPONENTIAL))
+    # lrd=DependentParameter( lambda re: np.log10( re ), rad )
+    # add_prior( lrd, 0.32*0.1/0.03, 0.25 )  ## good value: sig_lre=0.3
     
-    ratio = FreeParameter( 0.5, Range( (0.01, 1.0), RangeType.LINEAR) )
-    X_rd = DependentParameter( lambda q: np.log( (q-0.01)/(1.01-q) ), ratio )
-    add_prior( X_rd, 0.03, 1.0 ) 
+    lrd=DependentParameter( lambda re: 1.015**(re - 10), rad )
+    add_prior( lrd, 0.027/0.03,  0.5) 
     
-    sersic = FreeParameter(2.0, Range((0.3, 16.6), RangeType.LINEAR))
-    X_sersic = DependentParameter(lambda nt: 1.4**(nt - 7.), sersic)
-    add_prior(X_sersic, 0.0, 1.0)
+    if False:
+        sersic = FreeParameter( 2.0, Range((0.3, 8.0), RangeType.LINEAR))
+        X_sersic = DependentParameter( lambda n: np.log( (n-0.25)/(10-n) ), sersic )
+        add_prior( X_sersic, -0.8, 1.5 )
+    if True:
+        sersic = FreeParameter( 2.0, Range((0.3, 8.4), RangeType.LINEAR))
+        X_sersic = DependentParameter( lambda n: np.log( (n-0.25)/(10-n) ), sersic )
+        add_prior( X_sersic, -2.5, 1.5 )
 
-    # we can also get the WCS coordinates, and the radius, angle and ratio in angular units
-    # see https://sourcextractorplusplus.readthedocs.io/en/latest/config_api/model_fitting.html?highlight=get_world_parameters#config.model_fitting.get_world_parameters
+    e1 = FreeParameter( 0.0, Range((-0.9999, 0.9999), RangeType.LINEAR))
+    e2 = FreeParameter( 0.0, Range((-0.9999, 0.9999), RangeType.LINEAR))
+    emod = DependentParameter( lambda x,y: np.sqrt( x*x + y*y ), e1, e2 )
+    angle = DependentParameter( lambda e1,e2 : 0.5*np.arctan2( e1, e2 ), e1, e2 )
+    ratio = DependentParameter( lambda e : np.abs(1-e)/(1+e), emod )
+    add_prior( e1, 0.0, 0.25 )
+    add_prior( e2, 0.0, 0.25 )
+
+    
     ra, dec, wc_rad, wc_angle, wc_ratio = get_world_parameters(x, y, rad, angle, ratio)
     
-    # next, write these outputs 
+    add_output_column('X_MODEL', x)
+    add_output_column('Y_MODEL', y)
     add_output_column('RA_MODEL', ra)
     add_output_column('DEC_MODEL', dec)
     add_output_column('RADIUS', wc_rad)
     add_output_column('AXRATIO', wc_ratio)
     add_output_column('ANGLE', wc_angle)
+    add_output_column('E1',e1)
+    add_output_column('E2',e2)
     add_output_column('SERSIC', sersic)
     add_output_column('X_SERSIC', X_sersic)
     
-    ## The parameters above are fitted on all the images in the measurement group (all bands) in a S/N weighted average way (loosely speaking)
-    ## But the flux is fitted on each band separately (but we can actually 
-    
+    add_output_column('DET-IMG_RADIUS', rad)
+    add_output_column('DET-IMG_AXRATIO', ratio)
+    add_output_column('DET-IMG_ANGLE', angle)
+
+
     i = 0
     flux = {}
     mag = {}
-    # loop over each band in the measurement group
+    dx = {}
+    xx={}
+    dy = {}
+    yy={}
+
     for band,group in mesgroup: 
         print(band)
         
         flux[i] = get_flux_parameter()
         mag[i] = DependentParameter(lambda f, zp=mag_zeropoint[band]: -2.5 * np.log10(f) + zp, flux[i] )
         
-        # add all compontents to the final model that is going to be fitted
-        add_model(group, SersicModel(x, y, 
-            flux[i], rad, ratio, angle, sersic))
-        
-        # save output columns for the parameters fitted in each band, in this case the flux and magnitude
+        # the centroid is fixed for all bands
+        add_model(group, 
+                  SersicModel(x, y, flux[i], rad, ratio, angle, sersic ) )
+
         add_output_column(f'MAG_MODEL_{band}', mag[i])
         add_output_column(f'FLUX_MODEL_{band}', flux[i])
         i+=1
