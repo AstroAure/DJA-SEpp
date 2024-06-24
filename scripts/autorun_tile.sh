d003e5016f0d93f431a4e22f14b041d296c518ad
Aurélien Genin <aurelien.genin@cube-monde.net>
2024-06-24 15:32:39 +0200

    Implemented Auto AWS EC2 run tile

--- scripts/autorun_tile.sh
+++ scripts/autorun_tile.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# WIP
 
 FIELD=$1
 BUCKET=$2
@@ -9,22 +8,7 @@ THREAD_COUNT=${5:-32}
 BUCKET_FOLDER=${6:-'tiles'}
 TEMPLATE=${7:-'lt-0f2b50c4559cd895e'}
 
-# SCRIPT="#!/bin/bash
-# cd /home/ec2-user
-# echo 'Field : $FIELD' > /home/ec2-user/LOG.log
-# echo 'Tile  : $TILE' >> /home/ec2-user/LOG.log
-# echo 'Fit   : $FIT_CASE' >> /home/ec2-user/LOG.log
-# echo '' >> /home/ec2-user/LOG.log
-# echo 'Cloning git' >> /home/ec2-user/LOG.log
-# git clone --branch package https://github.com/AstroAure/DJA-SEpp.git
-# screen -S SEpp
-# echo 'Running SE++' >> /home/ec2-user/LOG.log
-# cd /home/ec2-user/DJA-SEpp/scripts
-# chmod +x setup.sh
-# chmod +x setup_image_psf.sh
-# chmod +x run_tile.sh
-# ./run_tile.sh $FIELD $BUCKET /home/ec2-user/RUN $TILE $FIT_CASE $THREAD_COUNT false false $BUCKET_FOLDER 1>> /home/ec2-user/LOG.log 2>> /home/ec2-user/LOG.log"
-#shutdown now -h"
+EC2_PATH='/home/ec2-user/miniconda3/envs/dawn-310/bin:/home/ec2-user/miniconda3/condabin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user:/home/ec2-user/.local/bin:/home/ec2-user/bin'
 
 SCRIPT="#!/bin/bash
 # sudo -u ec2-user -i
@@ -37,11 +21,8 @@ echo 'Cloning git' >> /home/ec2-user/LOG.log
 sudo -u ec2-user git clone --branch package https://github.com/AstroAure/DJA-SEpp.git
 echo 'Running SE++' >> /home/ec2-user/LOG.log
 cd /home/ec2-user/DJA-SEpp/scripts
-# sudo -u ec2-user /home/ec2-user/miniconda3/envs/dawn-310/bin/python3 -m pip install dja-sepp 1>> /home/ec2-user/LOG2.log 2>> /home/ec2-user/LOG2.log
-# sudo -u ec2-user /home/ec2-user/miniconda3/envs/dawn-310/bin/python3 download_psf.py $FIELD /home/ec2-user/RUN $BUCKET 1>> /home/ec2-user/LOG2.log 2>> /home/ec2-user/LOG2.log
-sudo -u ec2-user env "PATH=/home/ec2-user/miniconda3/envs/dawn-310/bin" ./setup_image_psf.sh $FIELD $BUCKET /home/ec2-user/RUN false $TILE false $BUCKET_FOLDER  1>> /home/ec2-user/LOG2.log 2>> /home/ec2-user/LOG2.log
-# sudo -u ec2-user env "PATH=/home/ec2-user/miniconda3/envs/dawn-310/bin" ./run_tile.sh $FIELD $BUCKET /home/ec2-user/RUN $TILE $FIT_CASE $THREAD_COUNT false false $BUCKET_FOLDER
-# sudo -u ec2-user screen -S SEpp -dm bash -c 'pip install dja-sepp; bash' "
+sudo -u ec2-user screen -S SEpp -dm bash -c 'sudo -u ec2-user env "PATH=$EC2_PATH" ./run_tile.sh $FIELD $BUCKET /home/ec2-user/RUN $TILE $FIT_CASE $THREAD_COUNT false false $BUCKET_FOLDER; sudo shutdown now -h'
+"
 
 echo "$SCRIPT"
 
