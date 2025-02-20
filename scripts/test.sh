02eef9c6bc91622aceaa6fc3b4380441db87b2d7
Aurélien Genin <aurelien.genin@cube-monde.net>
2025-02-20 16:42:04 +0100

    Added comparisons to other results

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-A=$1
-B=$2
-C=$3
+FILTER=$1
 
-echo $A
-echo $B
-echo $C
+aws s3 mv s3://aurelien-sepp/ceers-full-grizli-v7.2/sepp/B+D/checkimages/model_ceers-full-grizli-v7.2-$FILTER-clear_drc_sci_tile-full_1.fits.gz s3://aurelien-sepp/ceers-full-grizli-v7.2/sepp/B+D/checkimages/model_B+D_ceers-full-grizli-v7.2-$FILTER-clear_drc_sci_tile-full_1.fits.gz
+aws s3 mv s3://aurelien-sepp/ceers-full-grizli-v7.2/sepp/B+D/checkimages/resid_ceers-full-grizli-v7.2-$FILTER-clear_drc_sci_tile-full_1.fits.gz s3://aurelien-sepp/ceers-full-grizli-v7.2/sepp/B+D/checkimages/resid_B+D_ceers-full-grizli-v7.2-$FILTER-clear_drc_sci_tile-full_1.fits.gz
+aws s3 mv s3://aurelien-sepp/ceers-full-grizli-v7.2/sepp/sersic_rg4/checkimages/model_ceers-full-grizli-v7.2-$FILTER-clear_drc_sci_tile-full_1.fits.gz s3://aurelien-sepp/ceers-full-grizli-v7.2/sepp/sersic_rg4/checkimages/model_sersic_rg4_ceers-full-grizli-v7.2-$FILTER-clear_drc_sci_tile-full_1.fits.gz
+aws s3 mv s3://aurelien-sepp/ceers-full-grizli-v7.2/sepp/sersic_rg4/checkimages/resid_ceers-full-grizli-v7.2-$FILTER-clear_drc_sci_tile-full_1.fits.gz s3://aurelien-sepp/ceers-full-grizli-v7.2/sepp/sersic_rg4/checkimages/resid_sersic_rg4_ceers-full-grizli-v7.2-$FILTER-clear_drc_sci_tile-full_1.fits.gz
